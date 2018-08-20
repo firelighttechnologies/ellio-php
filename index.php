5228c09c5a749b6007b2e8b282ba57dff2265baa
anitabeetle <37273464+anitabeetle@users.noreply.github.com>
2018-08-19 22:04:04 -0500

    Switched to .php and querry tasks

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!doctype html>
 <html>
+<?
+include '../conn.php';
+$sql = "SELECT * FROM tasks";
+$result = mysqli_query($conn, $sql);
+?>
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Latest compiled and minified CSS -->
@@ -10,6 +15,18 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style media="screen">
+.row {
+    margin-right: -15px;
+    margin-left: -15px;
+    display: grid;
+    grid: none / auto auto auto auto auto auto auto auto auto auto auto auto;
+}
+.l1 {
+	display: none;
+	font-weight: bold;
+}
+</style>
 <meta charset="utf-8">
 <title>Untitled Document</title>
 </head>
@@ -26,6 +43,19 @@
     <div class="category">Category</div>
     <div class="Status">Status</div>
   </div>
+	<div>
+<?
+	if(mysqli_num_rows($result) > 0) {
+	// output data of each row
+	while($row = mysqli_fetch_assoc($result)) {
+			echo "<div class='l1'> task_id:</div> " . $row["task_id"]. " <div class='l1'> - created: </div>" . $row["created"]. " <div class='l1'>Category</div> - ". $row["category"]. " <div class='l1'>task</div> " . $row["task"]. " <div class='l1'>due</div> ". $row["due"]. " <div class='l1'>status:</div> " . $row["status"]. "<br>";
+	}
+} else {
+	echo "0 results";
+}
+mysqli_close($conn);
+?>
+	</div>
 </main>
 	<aside class="new">
 		<form>
